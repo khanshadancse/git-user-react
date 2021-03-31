8fdc5aba572c83496ad6b1251a92ccdcb1af306f
Shadan Khan <shadank7@yahoo.com>
2021-04-01 00:18:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const FetchGitUser = () => {
+  const [users,setUser] = useState([]);
+
+  const apiUrl = `https://api.github.com/users`;
+
+  const fetchedGitData = async () => {
+    const response = await fetch(apiUrl);
+    const users = await response.json();
+    setUser(users)
+  }
+  useEffect(()=>{
+    fetchedGitData();
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>{users.map((user)=>{
+      return (
+        <>
+       
+                  <li key={user.id}>
+                    <div class="user-main">
+                    <div className="user-profile">
+                      <figure>
+                        <img src={user.avatar_url}/>
+                      </figure>
+                    </div>
+                    <div className="user-details">
+                      <h4>{user.login}</h4>
+                      <a href={user.html_url} target="_blank">Profile</a>
+                    </div>
+                    </div>
+        </li>
+        </>
+      )
+    })}</>
+  )
+}
+const App = () => {
+  return (
+    <>
+    <h2 className="h2">Git Users</h2>
+    <div className="user-box">  
+        <ul className="user-container">
+    <FetchGitUser />
+    </ul>
     </div>
-  );
+
+    </>
+  )
 }
 
 export default App;
